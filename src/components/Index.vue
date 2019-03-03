ce7674e96972576cbf779ebbf14c610a88e71561
edwardzhong <edwardloveyou@gmail.com>
2019-03-03 16:05:27 +0800

    format

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,79 +1,85 @@
 <template>
-	<div>
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th>name</th>
-					<th>amount</th>
-					<th>operate</th>
-				</tr>
-			</thead>
-			<template v-for="item in list">
-				<tr>
-					<td>{{item.name}}</td>
-					<td>{{item.num}}</td>
-					<td>
-						<button class="button button-primary" @click="update(item)">edit</button>
-						<button class="button button-success" @click="remove(item.id)">delete</button>
-					</td>
-				</tr>
-			</template>
-		</table>
-		<edit-dialog></edit-dialog>
-		<tip></tip>
-	</div>
+  <div>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>name</th>
+          <th>amount</th>
+          <th>operate</th>
+        </tr>
+      </thead>
+      <template v-for="item in list">
+        <tr>
+          <td>{{item.name}}</td>
+          <td>{{item.num}}</td>
+          <td>
+            <button class="button button-primary" @click="update(item)">edit</button>
+            <button class="button button-success" @click="remove(item.id)">delete</button>
+          </td>
+        </tr>
+      </template>
+    </table>
+    <edit-dialog></edit-dialog>
+    <tip></tip>
+  </div>
 </template>
 <script>
-	import dialog from '../common/dialog.vue'
-	import tip from '../common/tip.vue'
+import dialog from "../common/dialog.vue";
+import tip from "../common/tip.vue";
 
-	export default {
-		computed:{
-			list:function(){
-				return this.$store.getters.list;
-			}
-		},
-		components:{ 
-			'edit-dialog':dialog,
-			tip 
-		},
-		created(){
-			this.$store.commit('init', 
-				[{
-					id:1,
-					name: 'Chuck Norris',
-					num: 10000
-				}, {
-					id:2,
-					name: 'Bruce Lee',
-					num: 9000
-				}, {
-					id:3,
-					name: 'Jackie Chan',
-					num: 7000
-				}, {
-					id:4,
-					name: 'Jet Li',
-					num: 8000
-				}]
-			);
-		},
-		methods:{
-			update(obj){
-				this.$store.commit('openDialog',obj);
-			},
-			remove(id){
-				let self=this;
-				this.$store.commit('openTip',{text:'确定删除该项 ？',callback:function(){
-					self.$store.commit('remove',id);
-					self.$store.commit('closeTip');
-				}});
-			}
-		},
-		filters: {
-			uperCase(value) {
-				return value.toUpperCase();
-			}
-		}
-	}
+export default {
+  computed: {
+    list: function() {
+      return this.$store.getters.list;
+    }
+  },
+  components: {
+    "edit-dialog": dialog,
+    tip
+  },
+  created() {
+    this.$store.commit("init", [
+      {
+        id: 1,
+        name: "Chuck Norris",
+        num: 10000
+      },
+      {
+        id: 2,
+        name: "Bruce Lee",
+        num: 9000
+      },
+      {
+        id: 3,
+        name: "Jackie Chan",
+        num: 7000
+      },
+      {
+        id: 4,
+        name: "Jet Li",
+        num: 8000
+      }
+    ]);
+  },
+  methods: {
+    update(obj) {
+      this.$store.commit("openDialog", obj);
+    },
+    remove(id) {
+      let self = this;
+      this.$store.commit("openTip", {
+        text: "确定删除该项 ？",
+        callback: function() {
+          self.$store.commit("remove", id);
+          self.$store.commit("closeTip");
+        }
+      });
+    }
+  },
+  filters: {
+    uperCase(value) {
+      return value.toUpperCase();
+    }
+  }
+};
 </script>
--- src/components/Sign.vue
+++ src/components/Sign.vue
@@ -1,75 +1,88 @@
 <template>
-	<form class="sign-form form form-aligned">
-		<div class="alert alert-danger" v-show="show">{{text}}</div>
-		<fieldset>
-			<div class="control-group">
-				<input type="text" class="input-1" placeholder="Username" v-model="user.name" v-bind:class="{invalid:nameInvalid}" v-focus="nameInvalid"/>
-			</div>
-			<div class="control-group">
-				<input type="password" class="input-1" placeholder="Password" v-model="user.pass" v-bind:class="{invalid:passInvalid}"  v-focus="nameInvalid"/>
-			</div>
-		</fieldset>
-		<button type="button" class="button input-1 button-info" @click="submit">Sign in</button>
-		<div class="alert alert-info">Username is admin, Password is 8888</div>
-	</form>
+  <form class="sign-form form form-aligned">
+    <div class="alert alert-danger" v-show="show">{{text}}</div>
+    <fieldset>
+      <div class="control-group">
+        <input
+          type="text"
+          class="input-1"
+          placeholder="Username"
+          v-model="user.name"
+          v-bind:class="{invalid:nameInvalid}"
+          v-focus="nameInvalid"
+        >
+      </div>
+      <div class="control-group">
+        <input
+          type="password"
+          class="input-1"
+          placeholder="Password"
+          v-model="user.pass"
+          v-bind:class="{invalid:passInvalid}"
+          v-focus="nameInvalid"
+        >
+      </div>
+    </fieldset>
+    <button type="button" class="button input-1 button-info" @click="submit">Sign in</button>
+    <div class="alert alert-info">Username is admin, Password is 8888</div>
+  </form>
 </template>
 <script>
-	export default {
-		data(){
-			return{
-				show:false,
-				nameInvalid:false,
-				passInvalid:false,
-				text:"",
-				user:{
-					name:'',
-					pass:''
-				}
-			}
-		},
-		directives: {
-			focus: {
-			  inserted: function (el,{value}) {
-			  	console.log(value);
-			    if(value){
-			    	el.focus();
-			    }
-			  }			
-			}
-		},
-		methods:{
-			submit(){
-				this.show=false;
-				this.text='';
-				this.nameInvalid=false;
-				this.passInvalid=false;
-				if(!this.user.name){
-					this.showError('the user name is null');
-					this.nameInvalid=true;
-					return false;
-				}
-				if(!this.user.pass){
-					this.showError('this user password is null');
-					this.passInvalid=true;
-					return false;
-				}
-				if(this.user.name!=='admin'){
-					this.showError('this user name is not exist');
-					this.nameInvalid=true;
-					return false;
-				}
-				if(this.user.pass!=='8888'){
-					this.showError('this user password is wrong');
-					this.passInvalid=true;
-					return false;
-				}
-				location.href='./index.html';
-
-			},
-			showError(text){
-				this.show=true;
-				this.text=text;
-			}
-		}
-	}
+export default {
+  data() {
+    return {
+      show: false,
+      nameInvalid: false,
+      passInvalid: false,
+      text: "",
+      user: {
+        name: "",
+        pass: ""
+      }
+    };
+  },
+  directives: {
+    focus: {
+      inserted: function(el, { value }) {
+        console.log(value);
+        if (value) {
+          el.focus();
+        }
+      }
+    }
+  },
+  methods: {
+    submit() {
+      this.show = false;
+      this.text = "";
+      this.nameInvalid = false;
+      this.passInvalid = false;
+      if (!this.user.name) {
+        this.showError("the user name is null");
+        this.nameInvalid = true;
+        return false;
+      }
+      if (!this.user.pass) {
+        this.showError("this user password is null");
+        this.passInvalid = true;
+        return false;
+      }
+      if (this.user.name !== "admin") {
+        this.showError("this user name is not exist");
+        this.nameInvalid = true;
+        return false;
+      }
+      if (this.user.pass !== "8888") {
+        this.showError("this user password is wrong");
+        this.passInvalid = true;
+        return false;
+      }
+      location.href = "./index.html";
+    },
+    showError(text) {
+      this.show = true;
+      this.text = text;
+    }
+  }
+};
 </script>
